cf446d3c6ee64e505e74a01c0cf2fefa04d54fb7
satyajit.nabeebacus <satyajit.nabeebacus@rapp.com>
2025-08-03 21:05:58 +0400

    add Zod and error handling on topic server action

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,5 +1,39 @@
 "use server";
 
-export async function createTopic() {
+import { error } from "console";
+import { z } from "zod";
+
+const createTopicShema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces.",
+    }),
+  description: z.string().min(1, "Description is required"),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   // TODO: revalidate the homepage after creating a topic
+
+  const result = createTopicShema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return { errors: z.flattenError(result.error).fieldErrors };
+  }
+
+  return { errors: {} };
 }
