207492976e95a0eb6400594bfdab9174149ae491
satyajit.nabeebacus <satyajit.nabeebacus@rapp.com>
2025-08-17 22:02:19 +0400

    add comments to post and handle db queries cache

--- discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,5 +1,29 @@
-function PostShowPage() {
-  return <div>Post Show Page</div>;
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import paths from "@/paths";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = await params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList postId={postId} />
+    </div>
+  );
 }
 
 export default PostShowPage;
